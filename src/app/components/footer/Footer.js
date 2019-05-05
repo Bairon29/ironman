f0df1a713cb0c762b7e60c95a85cff792b737d61
Bairon29 <baironvasquez29@gmail.com>
2019-05-05 17:10:02 -0400

    starting mobile stage

---
+++ src/app/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <section className="footer">
+        <p>&#9400; 2019 Iron Man</p>
+        <h1>Iron Man</h1>
+    </section>
+  );
+}
+
+export default Footer;
